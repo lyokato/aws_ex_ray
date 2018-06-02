a70b1a56ebd557c7d0be15d625e6a3825b5fdb4a
lyokato <lyo.kato@gmail.com>
2018-06-02 17:49:36 +0000

    ver 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AwsExRay.MixProject do
   def project do
     [
       app: :aws_ex_ray,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.6",
       package: package(),
       start_permanent: Mix.env() == :prod,
