3586c1e5c78e58a9824202ead90f3a1bb4d24154
lyokato <lyo.kato@gmail.com>
2019-06-03 11:36:04 +0900

    update ex_doc dependency version

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule AwsExRay.MixProject do
     [
       {:mox, "~> 0.3.2", only: :test},
       {:credo, "~> 0.3", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.15", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:poison, "~> 3.1"},
       {:poolboy, "~> 1.5"},
       {:struct_assert, "~> 0.5.2", only: :test},
