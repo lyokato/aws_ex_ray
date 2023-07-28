87afbf9c655595440983530b8d3ac476b73602ee
Magnus Henoch <magnus.henoch@gmail.com>
2023-07-28 18:26:34 +0100

    Fix sampling rate for test
    The tests assume that the sampling rate is zero.

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :aws_ex_ray,
-  sampling_rate: 1.0,
+  sampling_rate: 0.0,
   client_module: AwsExRay.Client.Sandbox,
   sandbox_sink_module: AwsExRay.Client.Sandbox.Sink.Stub,
   default_annotation: %{foo: "bar"},
